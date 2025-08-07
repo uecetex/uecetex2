a05569cde1ae53513f7c1f6ac7840ee6206ec01e
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2025-08-07 09:24:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 VERSION=v1.0.1
 CURRENT_DATE=`date +'%Y-%m-%d'`
 
-function compileLaTeX(){
+function compile_latex(){
 
     local file="$1"
 
@@ -12,26 +12,33 @@ function compileLaTeX(){
 
     echo "Compiling $texFile"
 
-    latexmk -pdf -time -silent -output-directory="$dir" $base
+    latexmk -pdf -halt-on-error -time -output-directory="$dir" $base
 }
 
 function initialize(){
 
-    # initializing: create empty directories
+    # Remove 'dist' folder and all its content if it exists
     rm -rf dist
 
-    # creating directories for CTAN zip
+    # Create directories for CTAN zip
     mkdir -p dist/uecetex2/{tex,doc}
 
-    # copying all abntex2source files
-    # mkdir -p target/abntex2source/
-    cp -rf doc tex dist/uecetex2/
+    # Copy all content from 'doc' folder
+    cp -rf doc dist/uecetex2/
 
+    # Copy all content from 'tex' folder
+    cp -rf tex dist/uecetex2/
+
+    # Copy the README.md file
     cp README.md dist/uecetex2
 }
 
+function is_macos() {
+    [[ "$(uname)" == "Darwin" ]]
+}
+
 function install(){
-    if [[ "$(uname)" == "Darwin" ]]; then
+    if is_macos; then
         mkdir -p ~/Library/texmf/tex/latex/uecetex2
         cp dist/uecetex2/tex/latex/uecetex2/* ~/Library/texmf/tex/latex/uecetex2/
     else
@@ -40,39 +47,39 @@ function install(){
     fi
 }
 
-function buildExamples(){
+function compiling_documentation(){
 
     echo "Compiling examples"
 
-    for file in dist/uecetex2/doc/latex/uecetex2/examples/*.tex; do
-        compileLaTeX "$file"
+    for file in dist/uecetex2/doc/latex/uecetex2/*.tex; do
+        compile_latex "$file"
     done
 }
 
 # Cross-platform sed in-place
-function findAndReplace() {
+function find_and_replace() {
 
     local pattern="$1"
     local file="$2"
 
-    if [[ "$(uname)" == "Darwin" ]]; then
+    if is_macos; then
         sed -i '' "$pattern" "$file"
     else
         sed -i "$pattern" "$file"
     fi
 }
 
-function replaceVariable(){
+function replace_variable(){
 
     local token="$1"
     local value="$2"
 
-    find dist \( -name "*.cls" -o -name "*.md" \) | while read -r file; do
-        findAndReplace "s|$token|$value|g" "$file"
+    find dist \( -name "*.cls" -o -name "*.md" -o -name "*.tex" \) | while read -r file; do
+        find_and_replace "s|$token|$value|g" "$file"
     done
 }
 
-function removeTempFiles(){
+function remove_latex_temp_files(){
 
     # Deletes all files in 'dist' except .tex and .pdf
 
@@ -83,14 +90,14 @@ function main(){
 
     initialize
 
-    replaceVariable "<VERSION>" $VERSION
-    replaceVariable "<CURRENT_DATE>" $CURRENT_DATE
+    replace_variable "<VERSION>" $VERSION
+    replace_variable "<CURRENT_DATE>" $CURRENT_DATE
 
     install
 
-    buildExamples
+    compiling_documentation
 
-    removeTempFiles
+    remove_latex_temp_files
 }
 
 main
